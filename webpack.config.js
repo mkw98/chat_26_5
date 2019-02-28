938871583754a4dc1e7661a4484ed9208dc73e35
Maria Wieckowska <mkw98@gazeta.pl>
2019-02-28 19:52:23 +0100

    edit scripts remove and add node modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const OptimizeJsPlugin = require('optimize-js-plugin');
+
 const plugins = [new HtmlWebpackPlugin({
     template: 'client/index.html',
     filename: 'index.html',
@@ -8,52 +9,52 @@ const plugins = [new HtmlWebpackPlugin({
 })];
 
 module.exports = (env) => {
-  const environment = env || 'production';
-  if (env === 'production') {
+    if (env === 'production') {
         plugins.push(
             new OptimizeJsPlugin({
                 sourceMap: false
             })
         )
-  }
-  return {
-    mode: environment,
-    entry: './client/index.js',
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'app.bundle.js'
-    },
-    devServer: {
-      proxy: {
-        '/socket.io': {
-          target: 'http://localhost:3000',
-          ws: true
-        }
-      }
-    },
-    plugins: plugins,
-    module: {
-      rules: [{
-            test: /\.js$/,
-            loader: "babel-loader",
-            options: {
-              plugins: env !== 'production' ? ["react-hot-loader/babel"] : []
-            }
-          },
-          {
-            test: /\.css$/,
-            use: [{
-              loader: 'style-loader'
-            },
-            {
-              loader: 'css-loader',
-              options: {
-                modules: true
-              }
-            }
-          ]
-        }
-      ]
     }
-  }
+    return {
+        mode: env || 'production',
+        entry: './client/index.js',
+        output: {
+            path: path.resolve(__dirname, 'public'),
+            filename: 'app.bundle.js'
+        },
+        devServer: {
+            proxy: {
+                '/socket.io': {
+                    target: 'http://localhost:3000',
+                    ws: true
+                }
+            }
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: "babel-loader",
+                    options: {
+                        plugins: env !== 'production' ? ["react-hot-loader/babel"] : []
+                    }
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        { loader: 'style-loader'},
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: true
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+        plugins
+    }
 };
