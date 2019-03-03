86c781f677f94abf5205599cd0ade35b4dd6de69
Maria Wieckowska <mkw98@gazeta.pl>
2019-03-03 10:18:11 +0100

    edit messagelist.js

--- client/MessageList.js
+++ client/MessageList.js
@@ -1,6 +1,12 @@
 import React from 'react';
-
 import styles from './MessageList.css';
+
+const Message = props => (
+  <div className={styles.Message}>
+    <strong>{props.from} :</strong>
+    <span>{props.text}</span>
+  </div>
+);
 
 const MessageList = props => (
   <div className={styles.MessageList}>
